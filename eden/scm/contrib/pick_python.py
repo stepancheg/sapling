ef068acfeea02725045bff78235a4de54eab8d5a
Stepan Koltsov <stepan.koltsov@gmail.com>
2025-02-25 12:36:51 +0000

    Back out "Enable using python3.12 at build time (#957)"
    Original commit changeset: 4a1d4f6f8e0d

--- eden/scm/contrib/pick_python.py
+++ eden/scm/contrib/pick_python.py
@@ -38,7 +38,6 @@ def main(args):
         + [
             p + EXE
             for p in [
-                "python3.12",
                 "python3.11",
                 "python3.10",
                 "python3.9",
